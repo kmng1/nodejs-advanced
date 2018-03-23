fc81d82ef10a4211286d2bd5b4e68080aa3a77d3
kmng1 <kmng@ieee.org>
2018-03-23 23:39:58 +0800

    Lecture 29 refactor for benchmarking with apache benchmark

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
+process.env.UV_THREADPOOL_SIZE = 1;  // default 4
 const cluster = require('cluster');
 
 if (cluster.isMaster) {
   cluster.fork();
   cluster.fork();
-  cluster.fork();
-  cluster.fork();
 } else {
   const express = require('express');
+  const crypto = require('crypto');
   const app = express();
 
-  function doWork(duration) {
-    const start = Date.now();
-    while(Date.now() - start < duration) {}
-  }
-
   app.get('/', (req, res) => {
-    doWork(15000);
-    res.send('Hi there');
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+      res.send('Hi there');
+    });
   });
 
   app.get('/fast', (req, res) => {
